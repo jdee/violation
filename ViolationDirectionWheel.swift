14b495ea4be33eeabe6522ec4659e41368adc82f
Jimmy Dee <jgvdthree@gmail.com>
2014-07-05 14:41:41 -0700

    Added a skeleton for the direction wheel

--- ViolationDirectionWheel.swift
+++ ViolationDirectionWheel.swift
@@ -15,18 +15,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 import UIKit
 
-class SecondViewController: UIViewController {
-                            
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+class ViolationDirectionWheel: UIControl {
 
 }
-
