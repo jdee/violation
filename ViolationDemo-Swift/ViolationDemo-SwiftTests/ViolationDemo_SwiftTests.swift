7b1d120f26c3299d9e4b9903be4d87b8f9d2ba4f
Jimmy Dee <jgvdthree@gmail.com>
2014-07-05 13:25:44 -0700

    Basic demo app with a shell of the directed press GR.

---
+++ ViolationDemo-Swift/ViolationDemo-SwiftTests/ViolationDemo_SwiftTests.swift
@@ -0,0 +1,35 @@
+//
+//  ViolationDemo_SwiftTests.swift
+//  ViolationDemo-SwiftTests
+//
+//  Created by Jimmy Dee on 7/5/14.
+//  Copyright (c) 2014 Violation Technology. All rights reserved.
+//
+
+import XCTest
+
+class ViolationDemo_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ ViolationDirectedPressGestureRecognizer.swift
@@ -0,0 +1,13 @@
+//
+//  ViolationDirectedPressGestureRecognizer.swift
+//  ViolationDemo-Swift
+//
+//  Created by Jimmy Dee on 7/5/14.
+//  Copyright (c) 2014 Violation Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViolationDirectedPressGestureRecognizer: UIGestureRecognizer {
+
+}
