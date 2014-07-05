3d24acba0e5d448ebfeca25a2f782fb490abbaf3
Jimmy Dee <jgvdthree@gmail.com>
2014-07-05 14:09:16 -0700

    Swift demo of GR.

---
+++ ViolationDemo-Swift/ViolationDemo-Swift/GestureViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class GestureViewController: UIViewController {
+
+    @IBOutlet var input : UIView
+    @IBOutlet var output : UIView
+
+    var star : UIImageView!
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let directedPressGestureRecognizer = ViolationDirectedPressGestureRecognizer(target:self, action: "handleDirectedPress:")
+        input.addGestureRecognizer(directedPressGestureRecognizer)
+
+        star = UIImageView(image: UIImage(named: "hammer_sickle_in_star"))
+    }
+
+    func handleDirectedPress(sender: ViolationDirectedPressGestureRecognizer) {
+        switch (sender.state) {
+        case .Cancelled, .Ended:
+            star.removeFromSuperview()
+        case .Began:
+            setStarFrame(sender)
+            output.addSubview(star)
+        default:
+            setStarFrame(sender)
+        }
+    }
+
+    func setStarFrame(sender: ViolationDirectedPressGestureRecognizer) {
+        let location = sender.locationInView(input)
+        star.frame.origin.x = location.x - star.frame.size.width * 0.5
+        star.frame.origin.y = location.y - star.frame.size.height * 0.5
+    }
+}
+
