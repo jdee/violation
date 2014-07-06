56a9c1ecbb39f4e347027477ea178feacd514d16
Jimmy Dee <jgvdthree@gmail.com>
2014-07-05 19:49:50 -0700

    Use right copy of ViolationDirectionWheel

--- ViolationDemo-ObjC/ViolationDirectionWheel.m
+++ ViolationDemo-ObjC/ViolationDirectionWheel.m
@@ -23,6 +23,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         [self addGestureRecognizer:[[ViolationDirectedPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleDirectedPress:)]];
+        [self drawWheel];
     }
     return self;
 }
@@ -68,6 +69,125 @@
     double x = location.x - 0.5*self.bounds.size.width;
     double y = 0.5*self.bounds.size.height - location.y;
     _direction = atan2(y, x);
+}
+
+- (void)drawWheel
+{
+    const double lineWidth = 4.0;
+    const double radius = 0.5*(MIN(self.bounds.size.width, self.bounds.size.height) - lineWidth);
+    const CGPoint center = CGPointMake(self.bounds.size.width*0.5, self.bounds.size.height*0.5);
+
+    // outer ring, tangent to bounds (in square view)
+    CAShapeLayer* shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:0.0 endAngle:2.0*M_PI clockwise:NO].CGPath;
+    shapeLayer.lineWidth = lineWidth;
+    shapeLayer.strokeColor = [UIColor blackColor].CGColor;
+    shapeLayer.frame = self.bounds;
+    shapeLayer.opaque = NO;
+    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor clearColor].CGColor;
+
+    [self.layer addSublayer:shapeLayer];
+
+    // filled annulus
+    UIBezierPath* path = [UIBezierPath bezierPathWithArcCenter:center radius:radius-0.5*lineWidth startAngle:0.0 endAngle:2.0*M_PI clockwise:NO];
+    [path addArcWithCenter:CGPointMake(self.bounds.size.width*0.5, self.bounds.size.height*0.5) radius:radius*0.3 startAngle:0.0 endAngle:2.0*M_PI clockwise:YES];
+
+    shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path.CGPath;
+    shapeLayer.lineWidth = 0.0;
+    shapeLayer.strokeColor = [UIColor clearColor].CGColor;
+    shapeLayer.frame = self.bounds;
+    shapeLayer.opaque = NO;
+    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5].CGColor;
+
+    [self.layer addSublayer:shapeLayer];
+
+    // inner ring
+    shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = [UIBezierPath bezierPathWithArcCenter:center radius:radius*0.3 startAngle:0.0 endAngle:2.0*M_PI clockwise:YES].CGPath;
+    shapeLayer.lineWidth = lineWidth;
+    shapeLayer.frame = self.bounds;
+    shapeLayer.opaque = NO;
+    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor clearColor].CGColor;
+    shapeLayer.strokeColor = [UIColor blackColor].CGColor;
+
+    [self.layer addSublayer:shapeLayer];
+
+    const double outerTriangleDistance = radius * 0.85;
+    const double innerTriangleDistance = radius * 0.45;
+    const double triangleHeight = outerTriangleDistance - innerTriangleDistance;
+    const double tanPiOver6 = tan(M_PI/6);
+
+    // north
+    path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(center.x, center.y-outerTriangleDistance)];
+    [path addLineToPoint:CGPointMake(center.x-triangleHeight*tanPiOver6, center.y-innerTriangleDistance)];
+    [path addLineToPoint:CGPointMake(center.x+triangleHeight*tanPiOver6, center.y-innerTriangleDistance)];
+
+    shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path.CGPath;
+    shapeLayer.lineWidth = 0.0;
+    shapeLayer.strokeColor = [UIColor clearColor].CGColor;
+    shapeLayer.frame = self.bounds;
+    shapeLayer.opaque = NO;
+    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor blackColor].CGColor;
+
+    [self.layer addSublayer:shapeLayer];
+
+    // south
+    path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(center.x, center.y+outerTriangleDistance)];
+    [path addLineToPoint:CGPointMake(center.x-triangleHeight*tanPiOver6, center.y+innerTriangleDistance)];
+    [path addLineToPoint:CGPointMake(center.x+triangleHeight*tanPiOver6, center.y+innerTriangleDistance)];
+
+    shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path.CGPath;
+    shapeLayer.lineWidth = 0.0;
+    shapeLayer.strokeColor = [UIColor clearColor].CGColor;
+    shapeLayer.frame = self.bounds;
+    shapeLayer.opaque = NO;
+    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor blackColor].CGColor;
+
+    [self.layer addSublayer:shapeLayer];
+
+    // east
+    path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(center.x+outerTriangleDistance, center.y)];
+    [path addLineToPoint:CGPointMake(center.x+innerTriangleDistance, center.y-tanPiOver6*triangleHeight)];
+    [path addLineToPoint:CGPointMake(center.x+innerTriangleDistance, center.y+tanPiOver6*triangleHeight)];
+
+    shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path.CGPath;
+    shapeLayer.lineWidth = 0.0;
+    shapeLayer.strokeColor = [UIColor clearColor].CGColor;
+    shapeLayer.frame = self.bounds;
+    shapeLayer.opaque = NO;
+    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor blackColor].CGColor;
+
+    [self.layer addSublayer:shapeLayer];
+
+    // west
+    path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(center.x-outerTriangleDistance, center.y)];
+    [path addLineToPoint:CGPointMake(center.x-innerTriangleDistance, center.y-tanPiOver6*triangleHeight)];
+    [path addLineToPoint:CGPointMake(center.x-innerTriangleDistance, center.y+tanPiOver6*triangleHeight)];
+
+    shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path.CGPath;
+    shapeLayer.lineWidth = 0.0;
+    shapeLayer.strokeColor = [UIColor clearColor].CGColor;
+    shapeLayer.frame = self.bounds;
+    shapeLayer.opaque = NO;
+    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
+    shapeLayer.fillColor = [UIColor blackColor].CGColor;
+
+    [self.layer addSublayer:shapeLayer];
 }
 
 @end
